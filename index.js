5d24f6a20dcbb46081595bfbc188b1fb3664bbbc
Howard Tyson <howard@howardtyson.com>
2013-03-18 22:04:38 +0000

    colorizing output for easier debugging.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var async = require('async');
 var _ = require('underscore');
+var color = require('colors');
 
 var DirectoryWatcher = function(path) {
   this.path = path;
@@ -13,25 +14,20 @@ DirectoryWatcher.prototype.watchList = [];
 DirectoryWatcher.prototype.setPath = function(path) {
   this.path = path;
 };
-DirectoryWatcher.prototype.getStat = function(path, next) {
-  fs.stat(this.path + '/' + path, function (error, stat){
-    next(error, stat);
-  });
-};
 DirectoryWatcher.prototype.recursiveWatch = function(path, watchList, depth, next) {
   if (!depth) {
     depth = 0;
   }
   console.log('called upon to log ' + path);
-  self = this;
+  var self = this;
   fs.readdir(path, function(err, files) {
-    for (file in files) {
-      var item = files[file];
+    for (i in files) {
+      var item = files[i];
       var callbackMaker = function(item) {
         var innerItem = item;
         return function(error, stat) {
           if (error) {
-            console.error('Deal with me! - ', innerItem);
+            console.error('Deal with me! - '.red, innerItem);
           }
           else {
             if (stat.isDirectory()) {
@@ -47,8 +43,10 @@ DirectoryWatcher.prototype.recursiveWatch = function(path, watchList, depth, nex
     }
   });
   self.watcher = fs.watch(path, self.changeHandler)
-  watchList.push(this);
-  console.log(depth);
+  watchList[path] = this;
+  var message = 'Currently there are ' + _.keys(watchList).length + ' watchers.';
+  console.log(message.green);
+  console.log('depth is now: '.blue + depth);
   if (next && next) {
     console.log('**************here');
     next(null);
@@ -61,12 +59,17 @@ DirectoryWatcher.prototype.watch = function() {
   });
 };
 DirectoryWatcher.prototype.changeHandler = function(change, file) {
-  console.log(change);
-  this.getStat(file, function(error, stat) {
+  self = this;
+  var path = self.path + '/' + file;
+  console.log(path + ': ' + change);
+  fs.stat(path, function(error, stat) {
     if (error) {
-      console.log('may have been deleted', error);
+      console.log(path, 'may have been deleted', error);
     }
     else {
+      if (stat.isDirectory()) {
+        self.recursiveWatch(path, self.watchList);
+      }
       console.log('may be a directory?', stat.isDirectory());
     }
   });
@@ -74,4 +77,5 @@ DirectoryWatcher.prototype.changeHandler = function(change, file) {
 
 
 watcher = new DirectoryWatcher('/root/test');
+//watcher = new DirectoryWatcher('/var/lib/templeton_backups');
 watcher.watch();
