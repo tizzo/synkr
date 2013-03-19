f0a10cd34727455f65eb076577ecc7dec965e69c
Howard Tyson <howard@howardtyson.com>
2013-03-19 19:23:41 +0000

    Updating change detection handling.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var DirectoryWatcher = require('./lib/DirectoryWatcher');
 var colors = require('colors');
+var _ = require('underscore');
 watcher = new DirectoryWatcher('/home/vagrant/test');
 watcher.on('log', function(data) {
   if (data.level != 'error') {
@@ -14,6 +15,19 @@ watcher.on('log', function(data) {
   else {
     logger(data.message);
   }
+});
+watcher.on('ready', function() {
+  console.log('watching totally started.'.green);
+});
+watcher.on('watchAdded', function(watch) {
+  var message = 'Currently there are ' + _.keys(watcher.watchList).length + ' watchers.';
+  self.info(message.green);
+});
+watcher.on('fileDeletion', function(path) {
+  console.log('deleted: ' + path);
+});
+watcher.on('fileChange', function(path) {
+  console.log('changed: ' + path);
 });
 watcher.watch(function() {
   console.log('watching totally started.'.cyan);
