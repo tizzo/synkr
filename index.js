2df003516c639a36405f9aec3ec530dc1b29b732
Howard Tyson <howard@howardtyson.com>
2013-03-19 17:15:25 +0000

    Refactoring to use an event emitter for logging and to share an event emitter among instances.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var DirectoryWatcher = require('./lib/DirectoryWatcher');
-
-
 watcher = new DirectoryWatcher('/home/vagrant/test');
-//watcher = new DirectoryWatcher('/var/lib/templeton_backups');
+watcher.on('log', function(data) {
+
+  if (data.level != 'error') {
+    logger = console.log;
+  }
+  else {
+    logger = console.error;
+  }
+  if (data.object != undefined) {
+    logger(data.message, data.object);
+  }
+  else {
+    logger(data.message);
+  }
+});
 watcher.watch();
